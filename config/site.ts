41edbcb1ed5600c9ac31c4baa62a576b0dfc161f
Agentseed <admin@agentseed.org>
2023-10-27 11:10:31 -0600

    fixed config

--- config/site.ts
+++ config/site.ts
@@ -2,63 +2,47 @@ export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
 	name: "Ladybug relief fund",
-	description: "Make beautiful websites regardless of your design experience.",
+	description: "placeholder",
 	navItems: [
-	{
-		label: "Home",
-		href: "/",
-	},
-    {
-      label: "Buy",
-      href: "/buy",
-    },
-    {
-      label: "Stats",
-      href: "/stats",
-    },
-    {
-      label: "About",
-      href: "/about",
-    }
+		{
+			label: "Home",
+			href: "/",
+		},
+		{
+			label: "Buy",
+			href: "/buy",
+		},
+		{
+			label: "Stats",
+			href: "/stats",
+		},
+		{
+			label: "About",
+			href: "/about",
+		}
 	],
 	navMenuItems: [
-	{
-		label: "Profile",
-		href: "/profile",
-	},
-	{
-		label: "Dashboard",
-		href: "/dashboard",
-	},
-	{
-		label: "Projects",
-		href: "/projects",
-	},
-	{
-		label: "Team",
-		href: "/team",
-	},
-	{
-		label: "Calendar",
-		href: "/calendar",
-	},
-	{
-		label: "Settings",
-		href: "/settings",
-	},
-	{
-		label: "Help & Feedback",
-		href: "/help-feedback",
-	},
-	{
-		label: "Logout",
-		href: "/logout",
-	},
+		{
+			label: "Home",
+			href: "/",
+		},
+		{
+			label: "Buy",
+			href: "/buy",
+		},
+		{
+			label: "Stats",
+			href: "/stats",
+		},
+		{
+			label: "About",
+			href: "/about",
+		},
 	],
 	links: {
 		docs: "https://nextui.org",
-    	sponsor: "/donate",
-    	donate: "/donate",
+		sponsor: "/donate",
+		donate: "/donate",
 		about: "/about"
 	},
 };
